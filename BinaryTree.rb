3157894c8675620eef222d3bd16d6d65aa96046e
Walters Mitch <mitchell_walters@Walterss-MacBook-Pro.local>
2016-01-07 15:05:40 -0800

    added a binary tree

---
+++ BinaryTree.rb
@@ -0,0 +1,54 @@
+
+
+class Node
+  attr_accessor :value, :right, :left
+  def initialize(value)
+    @value = value
+    @right = nil
+    @left = nil
+  end
+end
+
+
+class Tree
+  attr_accessor :root
+  def initialize(root)
+    @root = Node.new(root)
+  end
+
+  def append(value)
+    current_node = root
+    appended = false
+    while ! appended
+      if value > current_node.value
+        if current_node.right
+          current_node = current_node.right
+        else
+          current_node.right = Node.new(value)
+          appended = true
+        end
+      else
+        if current_node.left
+          current_node = current_node.left
+        else
+          current_node.left = Node.new(value)
+          appended = true
+        end
+      end
+    end
+  end
+
+end
+
+
+tree = Tree.new(50)
+
+tree.append(69)
+tree.append(75)
+tree.append(40)
+tree.append(30)
+
+puts tree.root.right.right.value
+
+puts tree.root.left.value
+puts tree.root.left.left.value
